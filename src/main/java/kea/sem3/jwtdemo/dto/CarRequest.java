b6c5449ef00c6e660d8e28742c61caf51eb45b6d
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-02-11 09:15:51 +0100

    Added: - CarRequest class - CarResponse class - CarService class
    Updated:
    - Test folder
    - Car class updated with discount

---
+++ src/main/java/kea/sem3/jwtdemo/dto/CarRequest.java
@@ -0,0 +1,17 @@
+package kea.sem3.jwtdemo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarRequest {
+    private String brand;
+    private String model;
+    private double pricePrDay;
+    private double discount;
+}
--- src/main/java/kea/sem3/jwtdemo/entity/Car.java
+++ src/main/java/kea/sem3/jwtdemo/entity/Car.java
@@ -1,9 +1,13 @@
 package kea.sem3.jwtdemo.entity;
 
+import kea.sem3.jwtdemo.dto.CarRequest;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -20,15 +24,31 @@ public class Car {
     @Column(nullable = false,length = 50)
     private String model;
     @Column(nullable = false)
-    private int pricePrDay;
+    private double pricePrDay;
 
-    public Car(String brand, String model, int pricePrDay) {
+    private double discount;
+
+    @CreationTimestamp
+    LocalDateTime created;
+
+    @UpdateTimestamp
+    LocalDateTime edited;
+
+    public Car(String brand, String model, double pricePrDay, double discount) {
         this.brand = brand;
         this.model = model;
         this.pricePrDay = pricePrDay;
+        this.discount = discount;
     }
 
     public Car() {
+    }
+
+    public Car(CarRequest body) {
+        this.brand = body.getBrand();
+        this.model = body.getModel();
+        this.pricePrDay = body.getPricePrDay();
+        this.discount = body.getBestDiscount();
     }
 
     @Override
@@ -39,9 +59,5 @@ public class Car {
                 ", model='" + model + '\'' +
                 ", pricePrDay=" + pricePrDay +
                 '}';
-    }
-
-    public int getId() {
-        return id;
     }
 }
